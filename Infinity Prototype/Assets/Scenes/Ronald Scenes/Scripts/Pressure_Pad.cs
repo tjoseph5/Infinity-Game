c0d5a75dfb4c08aa4e5de0ad67884662f0fb1c45
TheOnlyRealGamer <61198729+TheOnlyRealGamer@users.noreply.github.com>
2021-04-03 17:50:54 -0400

    Added Pressure Pad
    Pressure pad only partially works right now, but I think with a little tweaking it will work entirely.

---
+++ Infinity Prototype/Assets/Scenes/Ronald Scenes/Scripts/Pressure_Pad.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pressure_Pad : MonoBehaviour
+{
+    // This script just checks if the pressure pad is pressed down, by seeing if it entered the trigger
+    void OnTriggerEnter(Collider other)
+    {
+
+            Debug.Log("Pressure Pad Pushed Down");
+
+    }
+}
