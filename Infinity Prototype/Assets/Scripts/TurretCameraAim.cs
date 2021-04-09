81efeb65a93a5eebd265f16f2ed902130510a9e4
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-04-09 18:51:51 -0400

    grrr

--- Infinity Prototype/Assets/Scripts/TurretCameraAim.cs
+++ Infinity Prototype/Assets/Scripts/TurretCameraAim.cs
@@ -22,17 +22,33 @@ public class TurretCameraAim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if(player.playerState == PlayerMovement.PlayerState.Turret)
         {
             Vector2 rotation = lookRotation.action.ReadValue<Vector2>();
             Vector3 rotate = new Vector3(rotation.x, rotation.y, 0);
-            rotate = cameraMainTransform.up * rotate.x + cameraMainTransform.right * rotate.y;
-            rotate.z = 0f;
-            transform.Rotate(rotate * Time.deltaTime * cameraSpeed);
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,0);
+            //rotate.x = Mathf.Clamp(rotate.x, -3, 3);
+            //rotate.y = Mathf.Clamp(rotate.y, -3, 3);
+            //rotate = cameraMainTransform.up * rotate.x + cameraMainTransform.right * rotate.y;
+            //rotate.z = 0f;
+            //transform.Rotate(rotate * Time.deltaTime * cameraSpeed);
+            //transform.eulerAngles = new Vector3(transform.eulerAngles.x,transform.eulerAngles.y,transform.eulerAngles.z * 0);
 
-            //rotate.x = Mathf.Clamp(rotate.x, -10, 0);
-            //rotate.y = Mathf.Clamp(rotate.y, -40, 40);
+            Debug.Log(rotate);
+
+            if(rotate.x < 5 && rotate.x > -5)
+            {
+                if (rotate.y < 5 && rotate.y > -5)
+                {
+                    rotate.z = 0f;
+                    rotate = cameraMainTransform.up* rotate.x + cameraMainTransform.right * rotate.y;
+                    transform.Rotate(rotate * Time.deltaTime * cameraSpeed);
+                    transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z * 0);
+
+                }
+            }
+
+            
         }
         else if(player.playerState == PlayerMovement.PlayerState.Standard)
         {
