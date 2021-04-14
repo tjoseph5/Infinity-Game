8c2a0c466fe90bb4726fbf9c06e360ca39896faf
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-04-14 16:24:47 -0400

    Updated Ball Checker Script

--- Infinity Prototype/Assets/Scenes/Ronald Scenes/Scripts/Ball_Checker.cs
+++ Infinity Prototype/Assets/Scenes/Ronald Scenes/Scripts/Ball_Checker.cs
@@ -7,22 +7,81 @@ public class Ball_Checker : MonoBehaviour
     //Getting Spawn Point for ball
     public Transform Ball_Spawn;
     // Ball Checker is used on the balls in the Golf Room to check if they went down the correct pipe.
+
+    public enum BallColor { Red, Green, Yellow, Blue, Purple};
+    public BallColor ballColor = BallColor.Red;
+    [SerializeField] Material[] materials = new Material[5];
+
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        switch (ballColor)
+        {
+            case BallColor.Red:
+                if(tag == "Holdable")
+                {
+                    gameObject.GetComponent<MeshRenderer>().material = materials[0];
+                }
+                break;
+
+            case BallColor.Green:
+                if (tag == "Holdable")
+                {
+                    gameObject.GetComponent<MeshRenderer>().material = materials[1];
+                }
+                break;
+
+            case BallColor.Yellow:
+                if (tag == "Holdable")
+                {
+                    gameObject.GetComponent<MeshRenderer>().material = materials[2];
+                }
+                break;
+
+            case BallColor.Blue:
+                if (tag == "Holdable")
+                {
+                    gameObject.GetComponent<MeshRenderer>().material = materials[3];
+                }
+                break;
+
+            case BallColor.Purple:
+                if (tag == "Holdable")
+                {
+                    gameObject.GetComponent<MeshRenderer>().material = materials[4];
+                }
+                break;
+        }
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
         //Using OnTriggerEnter function to check when ball enters trigger at bottom of pipe
         //CompareTag to check if the ball is in the correct tube
-        if (other.CompareTag(this.tag))
+        if (other.GetComponent<Ball_Checker>().ballColor == this.ballColor)
         {
             //Destroy ball if correct
             Debug.Log("Correct Hole");
-            Destroy(gameObject);
+            if(tag == "Holdable")
+            {
+                Destroy(gameObject);
+            }
         }
         else
         {
             //if the pipe is incorrect the ball respawns
             Debug.Log("Incorrect Hole");
-            Instantiate(this, Ball_Spawn.position, Ball_Spawn.rotation);
-            Destroy(gameObject);
+            if (tag == "Holdable")
+            {
+                Instantiate(this, Ball_Spawn.position, Ball_Spawn.rotation);
+                Destroy(gameObject);
+            }
         }
     }
 }
