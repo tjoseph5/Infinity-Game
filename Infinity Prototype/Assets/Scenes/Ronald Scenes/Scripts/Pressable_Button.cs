0df197bd32e445dd881071ca251e8279342ff643
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2021-04-21 01:23:40 -0400

    Added button functionally

--- Infinity Prototype/Assets/Scenes/Ronald Scenes/Scripts/Pressable_Button.cs
+++ Infinity Prototype/Assets/Scenes/Ronald Scenes/Scripts/Pressable_Button.cs
@@ -6,16 +6,59 @@ public class Pressable_Button : MonoBehaviour
 {
     public Rigidbody rb;
     public int dir;
+
+    public GameObject[] lights;
+    public BoxCollider[] platforms;
+
+    bool lightOn;
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        if (lightOn)
+        {
+            foreach (GameObject light in lights)
+            {
+                light.SetActive(true);
+            }
+
+            foreach (BoxCollider platform in platforms)
+            {
+                platform.enabled = false;
+            }
+        }
+        else if (!lightOn)
+        {
+            foreach (GameObject light in lights)
+            {
+                light.SetActive(false);
+            }
+
+            foreach (BoxCollider platform in platforms)
+            {
+                platform.enabled = true;
+            }
+        }
+    }
+
     public void ButtonPress()
     {
         //put button stuff
         StartCoroutine("Press");
+
+        if (lightOn)
+        {
+            lightOn = false;
+        } 
+        else if (!lightOn)
+        {
+            lightOn = true;
+        }
+       
     }
     //this animates the button being pressed
     IEnumerator Press()
